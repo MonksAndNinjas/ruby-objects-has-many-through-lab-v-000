1257b0e036b6cfc647b21f83e5a3ebefede897c5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-30 18:12:12 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,8 +5,7 @@ class Appointment
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    doctor_instance = Doctor.new(doctor)
-    doctor_instance.add_appointment(self)
+    @doctor.add_appointment(self)
   end
 
 
